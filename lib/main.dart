04a9a9ac838c9fd6745bc4033cd800b5744198ed
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-10 22:53:10 +0600

    SingleChildScrollView add

--- lib/main.dart
+++ lib/main.dart
@@ -24,30 +24,39 @@ class MyApp extends StatelessWidget {
                 )),
           ),
           body: Center(
-            child: Container(
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .height / 5,
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .width / 4,
-              decoration: BoxDecoration(color: Colors.black12),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      color: Colors.yellow,
-                      child: Center(
-                        child: Text("Hello"),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Container(
+
+                width: MediaQuery.of(context).size.height / 4,
+                height: MediaQuery.of(context).size.width / 3,
+                decoration: BoxDecoration(color: Colors.black12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.yellow,
+                        child: Center(
+                          child: Text("Hello"),
+                        ),
                       ),
                     ),
-                  )
-                ],
+                    SizedBox(height: 10),
+                    Center(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.yellow,
+                        child: Center(
+                          child: Text("Hello"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
